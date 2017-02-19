b290ee5ea66e2e29225cd2ef84315ed2d6cc5142
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-19 17:35:26 -0500

    initial

---
+++ src/test-generator-node-bahmutov-spec.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/* global describe, it */
+describe('test-generator-node-bahmutov', () => {
+  const testGeneratorNodeBahmutov = require('.')
+  it('write this test', () => {
+    console.assert(testGeneratorNodeBahmutov, 'should export something')
+  })
+})
